18dadeb407f5df6c2527b1e383278b4ac9703aae
booton304 <41555101+booton304@users.noreply.github.com>
2024-11-05 16:19:44 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -91,7 +91,7 @@ export const managers = [
       "tradingScale": 10, // 1 - 10
       "preferredContact": "WhatsApp", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
-      {
+    {
       "roster": null,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "manager ID": "605653510083665920",
       "name": "Ben Dreckman",
