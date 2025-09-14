8f5eba0bf4d38bcd8b2234bfedb9912abcafa078
booton304 <41555101+booton304@users.noreply.github.com>
2025-09-14 02:44:28 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -171,7 +171,7 @@
             <li>The commissioner, or a majority vote of the league, reserves the right to invite new members to fill any vacated spots.</li>
             <li>Membership can be expanded with a majority vote.</li>
             <li>All members are expected to be active participants throughout the year, including setting a full lineup each week and engaging in league communications and votes.</li>
-        <ol>    
+<ol>    
     <h2 class="sectionHeading" bind:this={two}>Article II Financials</h2>
     
     <h3 bind:this={twoOne}>Section 2.1 Annual Buy-In</h3>
@@ -192,7 +192,7 @@
         <li>Managing the Sleeper fantasy app settings,</li>
         <li>Facilitating the annual rookie draft and offseason, and</li>
         <li>Acting as the final arbiter in any disputes.</li>
-    <ol>
+<ol>
 
     <h3 bind:this={threeTwo}>Section 3.2 Rule Changes</h3>
     <ol>        
@@ -201,7 +201,7 @@
         <li>All rule changes, with the exception of those related to collusion as detailed in Article VIII, require a majority approval vote (7 out of 12 teams) to be enacted.</li>
 
         <li>Rule changes will not take effect until the start of the following season, unless a time-sensitive issue requires an immediate vote and implementation (e.g., an unforeseen rule discrepancy).</li>
-    <ol>
+<ol>
 
     <h2 class="sectionHeading" bind:this={four}>Article IV Roster and Lineups</h2>
     
