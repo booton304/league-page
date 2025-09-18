b7a58e4cca193a669fbcdd1ced0ea6c1e533f77a
booton304 <41555101+booton304@users.noreply.github.com>
2025-09-17 23:54:49 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1181819510366093312"; // your league ID
 export const leagueName = "Boy's Club"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
